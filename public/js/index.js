2f836c172c331f8206422a49268c5ed012b8de45
erandro <erandro@gmail.com>
2018-09-23 20:58:42 -0500

    edit index.html and index.js - added the autocomplete to the js

--- public/js/index.js
+++ public/js/index.js
@@ -1,52 +1,168 @@
 $(document).ready(function () {
 
   // assign values to all elements that are called
+  // cards
   var homepageDiv = $("#home_page_div");
   var categoryDiv = $("#the_category_div");
   var languageDiv = $("#the_language_div");
   var idiomDiv = $("#the_idiom_div");
+  var multiDiv = $("#multi_res_div");
   var addIdiomDiv = $("#add_page_div");
-
+  // header
   var searchIdiomSubmit = $("#search_idiom_submit");
   var searchIdiomImput = $("#search_idiom_input");
+  // home gage card - category
+  var categoryButton = $(".category_button");
+  var searchCategorySubmit = $("#search_category_submit");
+  var searchCategoryImput = $("#search_category_input");
+  // home gage card - language
+  var languageButton = $(".language_button");
+  var searchLanguageSubmit = $("#search_language_submit");
+  var searchLanguageImput = $("#search_language_input");
+  // idiom card
   var addIdiomButton = $("#adding_button");
   var equivalentIdiom = $(".link_idiom");
-
-  var addIdiomSubmit = $("#submit_button");
+  // add idiom card
+  var addIdiomSubmit = $("#submit_idiom");
   var formLanguage = $("#language_form");
   var formIdiom = $("#idiom_form");
   var formPronunciation = $("#pronunciation_form");
   var formLiteral = $("#literal_form");
   var formMeaning = $("#meaning_form");
-  var formCategory = $("#category_form");
+  var formCategory = $("#category_form"); // delete (pass value from idiom card)
 
-  var categoryButton = $(".category_button");
-  var searchCategorySubmit = $("#search_category_submit");
-  var searchCategoryImput = $("#search_category_input");
-
-  var languageButton = $(".language_button");
-  var searchLanguageSubmit = $("#search_language_submit");
-  var searchLanguageImput = $("#search_language_input");
-
-  // Display changes action
+  // Cards display changes
   function hideAndShow(showTag) {
     if (showTag !== homepageDiv) {
-      homepageDiv.addClass("disable_class_name");
+      homepageDiv.addClass("hideDiv");
     }
     if (showTag !== categoryDiv) {
-      homepageDiv.addClass("disable_class_name");
+      categoryDiv.addClass("hideDiv");
     }
     if (showTag !== languageDiv) {
-      homepageDiv.addClass("disable_class_name");
+      languageDiv.addClass("hideDiv");
     }
     if (showTag !== idiomDiv) {
-      homepageDiv.addClass("disable_class_name");
+      idiomDiv.addClass("hideDiv");
     }
     if (showTag !== addIdiomDiv) {
-      homepageDiv.addClass("disable_class_name");
+      addIdiomDiv.addClass("hideDiv");
     }
-    showTag.removeClass("disable_class_name");
+    if (showTag !== multiDiv) {
+      multiDiv.addClass("hideDiv");
+    }
+    showTag.removeClass("hideDiv");
   }
+
+  // Autocomplete
+  function autocomplete(inp, arr) {
+    /*the autocomplete function takes two arguments,
+    the text field element and an array of possible autocompleted values:*/
+    var currentFocus;
+    /*execute a function when someone writes in the text field:*/
+    inp.addEventListener("input", function () {
+      var a, b, i, val = this.value;
+      /*close any already open lists of autocompleted values*/
+      closeAllLists();
+      if (!val) { return false; }
+      currentFocus = -1;
+      /*create a DIV element that will contain the items (values):*/
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+      /*append the DIV element as a child of the autocomplete container:*/
+      this.parentNode.appendChild(a);
+      /*for each item in the array...*/
+      for (i = 0; i < arr.length; i++) {
+        /*check if the item starts with the same letters as the text field value:*/
+        if (arr[i].substr(0, val.length).toUpperCase() === val.toUpperCase()) {
+          /*create a DIV element for each matching element:*/
+          b = document.createElement("DIV");
+          /*make the matching letters bold:*/
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+          /*insert a input field that will hold the current array item's value:*/
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          /*execute a function when someone clicks on the item value (DIV element):*/
+          b.addEventListener("click", function () {
+            /*insert the value for the autocomplete text field:*/
+            inp.value = this.getElementsByTagName("input")[0].value;
+            /*close the list of autocompleted values,
+            (or any other open lists of autocompleted values:*/
+            closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+    });
+    /*execute a function presses a key on the keyboard:*/
+    inp.addEventListener("keydown", function (e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) {
+        x = x.getElementsByTagName("div");
+      }
+      if (e.keyCode === 40) {
+        /*If the arrow DOWN key is pressed,
+        increase the currentFocus variable:*/
+        currentFocus++;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode === 38) { //up
+        /*If the arrow UP key is pressed,
+        decrease the currentFocus variable:*/
+        currentFocus--;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode === 13) {
+        /*If the ENTER key is pressed, prevent the form from being submitted,*/
+        e.preventDefault();
+        if (currentFocus > -1) {
+          /*and simulate a click on the "active" item:*/
+          if (x) {
+            x[currentFocus].click();
+          }
+        }
+      }
+    });
+    function addActive(x) {
+      /*a function to classify an item as "active":*/
+      if (!x) {
+        return false;
+      }
+      /*start by removing the "active" class on all items:*/
+      removeActive(x);
+      if (currentFocus >= x.length) {
+        currentFocus = 0;
+      }
+      if (currentFocus < 0) {
+        currentFocus = (x.length - 1);
+      }
+      /*add class "autocomplete-active":*/
+      x[currentFocus].classList.add("autocomplete-active");
+    }
+    function removeActive(x) {
+      /*a function to remove the "active" class from all autocomplete items:*/
+      for (var i = 0; i < x.length; i++) {
+        x[i].classList.remove("autocomplete-active");
+      }
+    }
+    function closeAllLists(elmnt) {
+      /*close all autocomplete lists in the document,
+      except the one passed as an argument:*/
+      var x = document.getElementsByClassName("autocomplete-items");
+      for (var i = 0; i < x.length; i++) {
+        if (elmnt !== x[i] && elmnt !== inp) {
+          x[i].parentNode.removeChild(x[i]);
+        }
+      }
+    }
+    /*execute a function when someone clicks in the document:*/
+    document.addEventListener("click", function (e) {
+      closeAllLists(e.target);
+    });
+  }
+  var idiomsNames = [];
+  autocomplete(document.getElementById("search_idiom_input"), idiomsNames);
 
   // The API methods
   $(function () {
@@ -56,21 +172,71 @@ $(document).ready(function () {
       event.preventDefault();
       // we should have the add button have an id of the idiom  
       var id = $(this).data("id");
-      hideAndShow(addIdiomDiv);
-      addIdiomSubmit.attr("data-id", id);
+      var category =
+        hideAndShow(addIdiomDiv);
+      addIdiomSubmit.attr("data-id", id).attr("data-category", category);
     });
 
-    // get all languages
-    $.ajax("/api/languages", {
+    // "get" all idioms and push to an array
+    $.ajax("/api/idioms", {
       type: "get"
     }).then(
       function (data) {
-        console.log(data);
-        // put data in array
+        data.forEach(function (element) {
+          idiomsNames.push(element.origin_idiom)
+        });
       }
     ).catch(function (err) {
       console.log(err);
     });
+
+    // "get" all languages and append to homepage drop-down
+    $.ajax("/api/languages", {
+      type: "get"
+    }).then(
+      function (data) {
+        data.forEach(function (element) {
+          searchLanguageImput.append(
+            `<option class="search_language_input"
+            data-id="${element.id}"
+            data-name="${element.language_name}"
+            data-abbreviation="${element.abbreviation}"
+            data-icon="${element.icon}"
+            >${element.language_name}</option>`
+          );
+          formLanguage.append(
+            `<option class="search_language_input"
+            data-id="${element.id}"
+            data-name="${element.language_name}"
+            data-abbreviation="${element.abbreviation}"
+            data-icon="${element.icon}"
+            >${element.language_name}</option>`
+          );
+        });
+      }
+    ).catch(function (err) {
+      console.log(err);
+    });
+
+    // get all categories and append to homepage drop-down
+    /*$.ajax("/api/categories", {
+      type: "get"
+    }).then(
+      function (data) {
+        data.forEach(function (element) {
+          searchCategoryImput.append(
+            `<option class="search_language_input"
+            data-id="${element.id}"
+            data-name="${element.language_name}"
+            data-abbreviation="${element.abbreviation}"
+            data-icon="${element.icon}"
+            >${element.language_name}</option>`
+          );
+        });
+      }
+    ).catch(function (err) {
+      console.log(err);
+    });*/
 
     // ajax calls:
 
@@ -79,20 +245,20 @@ $(document).ready(function () {
       event.preventDefault();
       // we should have the add button have an id of the idiom
       var id = $(this).data("id");
+      var idiomCategory = $(this).data("category");
       var newIdiom = {
         LanguageId: formLanguage, // send language id
         origin_idiom: formIdiom.val().trim(),
         pronunciation: formPronunciation.val().trim(),
         literal_meaning: formLiteral.val().trim(),
         meaning: formMeaning.val().trim(),
-        category: formCategory.val().trim(),
+        category: idiomCategory
       };
-      console.log(newIdiom);
       $.ajax("/api/idioms/" + id, {
         type: "post"
       }).then(
         function () {
-          addIdiomSubmit.removeAttr("data-id");
+          addIdiomSubmit.removeAttr("data-id").removeAttr("data-category");
           hideAndShow(idiomDiv);
           //getting an object with the info I need + Linked Idioms
         }
@@ -194,16 +360,7 @@ $(document).ready(function () {
 //test function area ******************* v down here v
 
 var testFunction = function () {
-  var id = "3";
-  $.ajax("/api/idiom/" + id, {
-    type: "get"
-  }).then(
-    function (data) {
-      console.log(JSON.stringify(data));
-    }
-  ).catch(function (err) {
-    console.log(err);
-  });
+  console.log("test");
 };
 
 testFunction();
